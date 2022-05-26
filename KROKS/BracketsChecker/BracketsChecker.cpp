ab391252d4047e73dec3b94ccd80b67117f2b1e7
Aleksey Boev <106151031+AbimAmalfi@users.noreply.github.com>
2022-05-26 16:40:10 +0300

    Add files via upload

--- KROKS/BracketsChecker/BracketsChecker.cpp
+++ KROKS/BracketsChecker/BracketsChecker.cpp
@@ -1,28 +1,25 @@
 ﻿#include <stdio.h>
 #define SIZE 256
 
+bool Check(char* message);
 int main(void)
 {
 	char message[SIZE];
 	fgets(message, SIZE, stdin);
+	printf("%d",Check(message));
+	return 0;
+}
+bool Check(char* message)
+{
 	unsigned short i = 0;
 	short bracketcheck = 0;
 	do
 	{
 		if ((message[i] == '(')) bracketcheck++;
-		
 		if (message[i] == ')') bracketcheck--;
-
-		if (bracketcheck < 0)
-		{
-			printf("false");
-			return 0;
-		}
+		if (bracketcheck < 0) return false;		
 		i++;
 	} while (message[i] != '\n');
-
-	if (bracketcheck == 0) printf("true");
-	else printf("false");
-
-	return 0;
+	if (bracketcheck == 0) return true;
+	else return false;
 }
---
+++ KROKS/IndexSearch/IndexSearch.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#define SIZE 256
+int search_index(int* array, int size, int value);
+int main()
+{
+	int array[SIZE];	
+	int value;
+	for (int i = 0; i < SIZE; ++i)
+	{
+		array[i] = rand() % 99;
+	}
+	for (int i = 0; i < SIZE - 1; i++) 
+	{
+		for (int j = 0; j < SIZE - i - 1; j++) 
+		{
+			if (array[j] > array[j + 1]) 
+			{
+				value = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = value;
+			}
+		}
+	}
+	char c;
+	value = getchar() - '0';
+	if ((c = getchar()) != '\n') value = (value*10) + (c - '0');
+
+	printf("index: %d\n",search_index(array, SIZE, value));
+	return 0;
+}
+
+int search_index(int* array, int size, int value)
+{
+	int left_boundary = 0; 
+	int right_boundary = size; 
+	int mid;
+
+	while (left_boundary <= right_boundary) 
+	{
+		mid = (left_boundary + right_boundary) / 2;
+		if (array[mid] == value) return mid;
+		if (array[mid] > value) right_boundary = mid - 1;
+		else left_boundary = mid + 1;
+	}
+	return -1;
+}
