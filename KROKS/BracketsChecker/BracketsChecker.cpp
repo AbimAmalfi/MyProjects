076a0a065c56d27608ffbdae3464d58de9bb5bee
Aleksey Boev <106151031+AbimAmalfi@users.noreply.github.com>
2022-05-26 15:02:20 +0300

    Add files via upload

---
+++ KROKS/BracketsChecker/BracketsChecker.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#define SIZE 256
+
+int main(void)
+{
+	char message[SIZE];
+	fgets(message, SIZE, stdin);
+	unsigned short i = 0;
+	short bracketcheck = 0;
+	do
+	{
+		if ((message[i] == '(')) bracketcheck++;
+		
+		if (message[i] == ')') bracketcheck--;
+
+		if (bracketcheck < 0)
+		{
+			printf("false");
+			return 0;
+		}
+		i++;
+	} while (message[i] != '\n');
+
+	if (bracketcheck == 0) printf("true");
+	else printf("false");
+
+	return 0;
+}
