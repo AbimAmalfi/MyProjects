813ee7f89328fb3a3cc4dfb0f0e455b4732e015c
Aleksey Boev <106151031+AbimAmalfi@users.noreply.github.com>
2022-05-26 12:43:46 +0300

    Add files via upload

---
+++ Client Server/Client/Client.cpp
@@ -0,0 +1,165 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <stdio.h>
+#include <ctime>
+#include <sstream>
+#pragma comment(lib, "Ws2_32.lib") //комментарий линкеру
+
+int Error_code(int x);
+int main()
+{
+	time_t timenow = time(0);
+	printf("Client.exe \nBoev A.Y. \n%s", ctime(&timenow));
+
+	WSADATA     wsadata;
+	WORD Version;
+	Version = MAKEWORD(2, 2);
+	SOCKADDR_IN TCPaddress{};
+	TCPaddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	TCPaddress.sin_family = AF_INET;
+	TCPaddress.sin_port = htons(5555);
+
+	if (WSAStartup(Version, &wsadata)) { Error_code(1); }
+	else printf("WinSock started\n");
+
+	SOCKET TCPsocket;
+	TCPsocket = socket(AF_INET, SOCK_STREAM, NULL);
+	if (TCPsocket == INVALID_SOCKET) { Error_code(2); }
+	else printf("TCPSocket created\n");
+
+	printf("Trying to connect...\n");
+	if (connect(TCPsocket, (SOCKADDR*)&TCPaddress, sizeof(TCPaddress))) { Error_code(4); }
+	else printf("Connected to a server\n");
+
+	char port[5];
+	char filename[25];
+	char message[30];
+
+	printf("Write desired port:");
+	if (scanf("%s", &port) == 0) { Error_code(10); }
+	printf("Write file name:");
+	if (scanf("%s", &filename) == 0) { Error_code(10); }
+	strcpy(message, port);
+	strcat(message, " ");
+	strcat(message, filename);
+	strcat(message, ".txt");
+	if (send(TCPsocket, message, 40, 0) == SOCKET_ERROR) { Error_code(5); }
+	char confirmation[40];
+	if (recv(TCPsocket, confirmation, 40, 0) == SOCKET_ERROR) { Error_code(6); }
+	printf("%s", confirmation);
+	u_short uport;
+	std::stringstream str(message);
+	str >> uport;
+	SOCKET UDPsocket;
+	UDPsocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (UDPsocket == INVALID_SOCKET) { Error_code(7); }
+	else printf("UDPSocket created\n");
+	SOCKADDR_IN UDPaddress{};
+
+	
+	int sizeofaddr = sizeof(UDPaddress);
+	UDPaddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	UDPaddress.sin_family = AF_INET;
+	UDPaddress.sin_port = htons(0);
+	if (bind(UDPsocket, (SOCKADDR*)&UDPaddress, sizeof(UDPaddress)) == SOCKET_ERROR) { Error_code(8); }
+	else printf("UDPSocket binded\n");
+	UDPaddress.sin_port = htons(uport);
+
+	int tm = 500;
+	unsigned int  sz = sizeof(tm);
+	if (setsockopt(TCPsocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&tm, sz)) { Error_code(3); }
+	else printf("recv timeout set\n");
+
+	FILE* input_file = fopen("logfile.txt", "r");
+	memset(confirmation, 0, sizeof(confirmation));
+
+	if (input_file != 0)
+	{
+		char buffer[1032];
+		char idbuffer[1036];
+		int truesize;
+		int i = 1;
+		while ((truesize = fread(buffer, 1, 1024, input_file)) != 0)
+		{
+			sprintf(idbuffer, "%d ", i);
+			strncat(idbuffer, buffer, truesize);
+			idbuffer[1026] = '\0';
+
+			sendto(UDPsocket, (char*)idbuffer, truesize + 12, 0, (SOCKADDR*)&UDPaddress, sizeof(UDPaddress));
+			printf("Packet %d sent\n",i);
+			while ((recv(TCPsocket, confirmation, 40, 0))<=0)
+			{
+				printf("No confirmation\n");
+				if (sendto(UDPsocket, (char*)idbuffer, truesize + 12, 0, (SOCKADDR*)&UDPaddress, sizeof(UDPaddress)) == SOCKET_ERROR) { Error_code(9); }
+				else printf("Packet %d resent\n",i);
+			}
+			
+
+			while (strcmp(confirmation, "Server:badpacket") == 0)
+			{
+				if (sendto(UDPsocket, (char*)idbuffer, truesize + 12, 0, (SOCKADDR*)&UDPaddress, sizeof(UDPaddress)) == SOCKET_ERROR) { Error_code(9); }
+				printf("%s\n", confirmation);
+			}
+
+			printf("%s\n", confirmation);
+			memset(confirmation, 0, sizeof(confirmation));
+			memset(buffer, 0, sizeof(buffer));
+			i++;
+
+
+		}
+		if (sendto(UDPsocket, "done\n", 6, 0, (SOCKADDR*)&UDPaddress, sizeof(UDPaddress)) == SOCKET_ERROR) { Error_code(9); }
+		else printf("\n\nServer notified, process terminated\n");
+
+	}
+
+
+
+	closesocket(TCPsocket);
+	closesocket(UDPsocket);
+	WSACleanup();
+
+	return 0;
+}
+
+int Error_code(int x)
+{
+	switch (x)
+	{
+	case 1:
+		printf("WSAstartup failed\n");
+		break;
+	case 2:
+		printf("TCPSocket is not created\n");
+		break;
+	case 3:
+		printf("Timeout set failed\n");
+		break;
+	case 4:
+		printf("Could not connect\n");
+		break;
+	case 5:
+		printf("Could not send\n");
+		break;
+	case 6:
+		printf("recv failed\n");
+		break;
+	case 7:
+		printf("UDPSocket is not created\n");
+		break;
+	case 8:
+		printf("UDPSocket could not bind\n");
+		break;
+	case 9:
+		printf("sendto failed\n");
+		break;
+	case 10:
+		printf("invalid data input\n");
+		break;
+	}
+	printf("Error code: %d\n", WSAGetLastError());
+	WSACleanup();
+	return 0;
+}
---
+++ Client Server/Server/Server.cpp
@@ -0,0 +1,148 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <stdio.h>
+#include <ctime>
+#include <sstream>
+#pragma comment(lib, "Ws2_32.lib") //комментарий линкеру
+
+int Error_code(int x);
+
+int main()
+{
+	time_t timenow = time(0);
+	printf("Server.exe \nBoev A.Y. \n%s", ctime(&timenow));
+
+	WSADATA     wsadata;
+	WORD Version;
+	Version = MAKEWORD(2, 2);
+	SOCKADDR_IN address{},udp_address{};
+	int sizeofaddr = sizeof(address);
+	address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	address.sin_family = AF_INET;
+	address.sin_port = htons(5555);
+
+	if (WSAStartup(Version, &wsadata)) { Error_code(1); }
+	else printf("WinSock started\n");
+
+	SOCKET ListenTCPsocket;
+	ListenTCPsocket = socket(AF_INET, SOCK_STREAM, NULL);
+	if (ListenTCPsocket == INVALID_SOCKET) { Error_code(2); }
+	else printf("TCPSocket created\n");
+
+	if (bind(ListenTCPsocket, (SOCKADDR*)&address, sizeof(address)) == SOCKET_ERROR) { Error_code(3); }
+	else printf("Binded succefully\n");
+	bool port_filename_confirmation = true;
+	char port_and_filename[30];
+	char name[25];
+	while (true)
+	{
+		if (listen(ListenTCPsocket, 2) == SOCKET_ERROR) { Error_code(4); }
+		else printf("Listening...\n");
+
+		SOCKET Connection;
+		if ((Connection = accept(ListenTCPsocket, (SOCKADDR*)&address, &sizeofaddr)) == INVALID_SOCKET) { Error_code(5); }
+		else printf("Client connected\n");
+
+		recv(Connection, port_and_filename, 30, 0);
+		u_short port;
+		std::stringstream str(port_and_filename);
+		str >> port;
+		str >> name;
+		printf("Received udpport: %hu\n", port);
+		printf("Received name: %s\n", name);
+		FILE* output_file = fopen(name, "w");
+		
+		SOCKET UDPsocket = socket(AF_INET, SOCK_DGRAM, 0);
+		int sizeofaddr = sizeof(udp_address);
+		udp_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+		udp_address.sin_family = AF_INET;
+		udp_address.sin_port = htons(port);
+		bind(UDPsocket, (SOCKADDR*)&udp_address, sizeof(udp_address));
+
+
+		char data[2048];
+		char* bigdata = new char[10485760];
+		int msg_lenght;
+		int output_lenght=0;
+		bool strnflag = true;
+		char id[1];
+		char response[40];
+		if (port_filename_confirmation) send(Connection, "Server:received port and filename\n", 40, 0);
+		port_filename_confirmation = false;
+		while(true)
+		{
+			msg_lenght = recvfrom(UDPsocket, data, 2048, 0, (SOCKADDR*)&udp_address, &sizeofaddr);
+		//	while ((msg_lenght = recvfrom(UDPsocket, data, 2048, 0, (SOCKADDR*)&udp_address, &sizeofaddr)) == INVALID_SOCKET)
+		//	{
+		//		send(Connection, "Server:badpacket", 40, 0);
+		//	}
+			if (strcmp(data, "done\n") == 0)
+			{
+				printf("Client sent all of the packages\n");
+				port_filename_confirmation = true;
+				break;
+			}
+			output_lenght = output_lenght + msg_lenght - 12;
+			id[0] = data[0];
+			memcpy(data, data + 2 , msg_lenght);
+			if(strnflag)
+			{ 
+				strncpy(bigdata, data, msg_lenght);
+				strnflag = false;
+			}
+			else strncat(bigdata, data, msg_lenght);
+			//printf("%s", data);
+			printf("Received package Number: %.1s\n", id);
+			strcpy(response, "Server:confirmed ");
+			strncat(response, id, 1);
+			send(Connection, response, 40, 0);
+			memset(data, NULL, sizeof(data));
+			
+		}
+		fwrite(bigdata, 1, output_lenght, output_file);
+		fclose(output_file); 
+		if (send(Connection, "check", 40, 0))
+		{
+			printf("Client disconected\n");
+		}
+		else printf("Сlient still hasn't disconnected, problem\n");
+
+		delete[] bigdata;
+	}
+
+	return 0;
+
+}
+
+
+
+
+int Error_code(int x)
+{
+	switch (x)
+	{
+	case 1:
+		printf("WSAstartup failed\n");
+		break;
+	case 2:
+		printf("Socket is not created\n");
+		break;
+	case 3:
+		printf("could not bind\n");
+		break;
+	case 4:
+		printf("can not listen\n");
+		break;
+	case 5:
+		printf("Failed to accept client connection\n");
+		break;
+	
+	}
+	printf("Error code: %d\n", WSAGetLastError());
+	return 0;
+}
+
+
